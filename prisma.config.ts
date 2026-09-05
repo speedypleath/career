6b5ea2c540db7e41d60e394734ecf49461b714ab
speedypleath <gheorgheandrei13@gmail.com>
2026-09-06 02:09:02 +0300

    debug tools

--- prisma.config.ts
+++ prisma.config.ts
@@ -11,6 +11,6 @@ import { defineConfig, env } from "prisma/config"
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.PRISMA_DATABASE_URL ?? env("DATABASE_URL"),
   },
 })
